28f547c2d23ccfddf0eb7165497cad2d552dca44
pgl <1801688332@qq.com>
2022-03-05 20:01:13 +0800

    fix: change 单引号

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,19 +1,19 @@
-const path = require("path");
+const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 清理上一次缓存文件
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index",
+  mode: 'development',
+  entry: './src/index',
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
   },
   // 支持多种文件
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"]
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
   },
   // 生产环境打包优化, 错误跟踪
   devtool: 'source-map',
@@ -24,16 +24,16 @@ module.exports = {
         test: /\.tsx?$/,
         // 如果用了ts文件,使用下面loader翻译成js
         // loader: 'awesome-typescript-loader'
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/, // 表示node_modules的tsx文件不做处理
       },
       {
         test: /\.(css|less)$/,
-        use: ["style-loader", "css-loader", "less-loader"],
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
       {
         test: /\.less$/,
-        loader: "less-loader",
+        loader: 'less-loader',
         options: {
           lessOptions: {
             javascriptEnabled: true
@@ -48,7 +48,7 @@ module.exports = {
     //   directory: path.join(__dirname, 'src')
     // },
     port: 8091,
-    host: "127.0.0.1",
+    host: '127.0.0.1',
     open: true,
     client: {
       progress: true, // 浏览器中以百分比显示编译进度
